212c2acd13eb5c369c471dfc5dbbe021af035bd0
zack <302904568@qq.com>
2019-11-18 09:29:55 +0800

    v0.0.1

---
+++ config/head_import.js
@@ -0,0 +1,6 @@
+
+document.write('<link href="' + dotsSum + 'bootstrap/css/bootstrap.min.css" rel="stylesheet">')
+document.write('<link href="' + dotsSum + 'components/common-footer/common-footer.css" rel="stylesheet">')
+document.write('<link href="' + dotsSum + 'components/common-head/common-head.css" rel="stylesheet">')
+document.write('<link rel="stylesheet" href="' + dotsSum + 'config/animate.min.css">')
+document.write("<script src = 'https://cdn.polyfill.io/v2/polyfill.min.js'></script>")
